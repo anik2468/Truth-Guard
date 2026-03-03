7c6c3787e7fcbb4817de093e35a72e32e53ceb58
Aniket Maurya <aniketmaurya64768@gmail.com>
2026-03-03 08:20:43 +0530

    Add fake news detection feature

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,50 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+# Allow CORS for frontend communication
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class NewsInput(BaseModel):
+    text: str
+
+fake_keywords = [
+    "breaking",
+    "shocking",
+    "secret cure",
+    "forward this",
+    "government hiding"
+]
 
 @app.get("/")
 def home():
     return {"message": "TruthGuard backend running"}
+
+@app.post("/check-news")
+def check_news(data: NewsInput):
+
+    text = data.text.lower()
+
+    score = 100
+
+    for word in fake_keywords:
+        if word in text:
+            score -= 20
+
+    if score < 60:
+        result = "⚠ Likely Fake News"
+    else:
+        result = "✅ Possibly Real"
+
+    return {
+        "result": result,
+        "credibility_score": score
+    }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,50 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import axios from "axios";
 
-function App() {
+function App(){
 
-  const [msg, setMsg] = useState("");
+const [text,setText] = useState("")
+const [result,setResult] = useState("")
+const [score,setScore] = useState("")
 
-  useEffect(() => {
-    axios.get("http://127.0.0.1:8000")
-      .then(res => {
-        setMsg(res.data.message);
-      });
-  }, []);
+const checkNews = async () => {
 
-  return (
-    <div style={{textAlign:"center", marginTop:"100px"}}>
-      <h1>TruthGuard</h1>
-      <h2>{msg}</h2>
-    </div>
-  );
+const res = await axios.post(
+"http://127.0.0.1:8000/check-news",
+{ text:text }
+)
+
+setResult(res.data.result)
+setScore(res.data.credibility_score)
+
 }
 
-export default App;
+return(
+
+<div style={{textAlign:"center",marginTop:"100px"}}>
+
+<h1>TruthGuard Fake News Detector</h1>
+
+<textarea
+rows="6"
+cols="50"
+placeholder="Paste news text here"
+onChange={(e)=>setText(e.target.value)}
+></textarea>
+
+<br/><br/>
+
+<button onClick={checkNews}>
+Analyze News
+</button>
+
+<h2>{result}</h2>
+<h3>Credibility Score: {score}</h3>
+
+</div>
+
+)
+
+}
+
+export default App
